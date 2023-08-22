d7b51e3e397caa7b3a6c860503dc426064e9cc15
Ilkhomjon2604 <m347es@gmail.com>
2023-08-22 12:06:15 +0200

    footer is added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from './components/nav'
 import Map from './components/map'
 import Card from './components/card/card'
 import Newspaper from './components/newspaper';
+import Footer from './components/footer';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Map/>
       <Card/>
       <Newspaper/>
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Footer extends React.Component{
+    render(){
+        return(
+            <div className="footer">
+               <span className="footer-text">
+© Copyright 2022. All Rights Reserved by LabArtisan</span>
+            </div>
+        )
+    }
+}
+
+export default Footer;
